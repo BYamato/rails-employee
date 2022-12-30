f47a39b43bb30b2f77719c2ee588bc3bdba9fbab
Yamato <yamato.belhouchet@yahoo.com>
2022-12-30 01:04:55 +0100

    faire design

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,4 +10,23 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
   end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :surname)
+  end
 end
